483ce642989b68f24a19529f234c5ada2985f4fc
Ric-toledo <rictols10@gmail.com>
2024-06-15 09:26:00 +0900

    Project's Initial Commit

---
+++ www/components/Plugin.js
@@ -0,0 +1,35 @@
+/* This is a JavaScript file*/
+
+$(document).on("click", "#Camera", function()
+{
+navigator.camera.getPicture(onSuccess, onFail, 
+{ quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+      correctOrientation: true,
+    saveToPhotoAlbum: true
+     });
+
+   
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+});
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
